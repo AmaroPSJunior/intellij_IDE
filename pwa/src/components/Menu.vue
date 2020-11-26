c5238373825b9ef2178bf8d142d77cf137e7561d
Amaro <amaro@solutiontrue.com.br>
2020-11-26 17:09:09 -0300

    code partial

---
+++ pwa/src/components/Menu.vue
@@ -0,0 +1,47 @@
+<template>
+  <q-item
+    clickable
+    tag="a"
+    :to="link"
+  >
+    <q-item-section
+      v-if="icon"
+      avatar>
+      <q-icon :name="icon" />
+    </q-item-section>
+
+    <q-item-section>
+      <q-item-label>{{ nome }}</q-item-label>
+      <q-item-label caption>
+        {{ uri }}
+      </q-item-label>
+    </q-item-section>
+  </q-item>
+</template>
+
+<script>
+export default {
+  name: 'Menu',
+  props: {
+    nome: {
+      type: String,
+      required: true,
+    },
+
+    uri: {
+      type: String,
+      default: '#',
+    },
+
+    link: {
+      type: String,
+      default: 'pessoas',
+    },
+
+    icon: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
